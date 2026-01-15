3c1086419bd881f1b2b40dc4c18ed9b658963bde
minjae25 <ihpubad22@inha.edu>
2026-01-15 22:53:23 +0900

    Title: 사칙연산 함수, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260115/사칙연산 함수/quadratic-operations-function.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int a;
+int c;
+char o;
+
+int add(){
+    return a+c;
+}
+int sub(){
+    return a-c;
+}
+int mul(){
+    return a*c;
+}
+int div(){
+    return a/c;
+}
+
+int main() {
+    cin >> a >> o >> c;
+
+    // Please write your code here.
+    if(o == '+'){
+        cout << a << " + " << c << " = " << add();
+    }
+    else if(o == '-'){
+        cout << a << " - " << c << " = " << sub();
+    }
+    return 0;
+}
