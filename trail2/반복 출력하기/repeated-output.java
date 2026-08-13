605b42059f6e05de6242488fe6435dfbe6b5e7d1
Minjae Cho <ihpubad22@inha.edu>
2026-08-13 19:38:35 +0900

    [Passed] 반복 출력하기 (127ms, 10MB)

---
+++ trail2/반복 출력하기/repeated-output.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void blah(){
+        System.out.println("12345^&*()_");
+    }
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        for(int i = 0; i < n; i++){
+            blah();
+        }
+    }
+}
