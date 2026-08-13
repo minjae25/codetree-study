6d54483f789aa5a58d48d9a608d1a2a333d8abd8
Minjae Cho <ihpubad22@inha.edu>
2026-08-13 18:01:31 +0900

    [Passed] 별 찍는 것을 5번 반복하기 (94ms, 9MB)

---
+++ trail2/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void print10Stars(){
+        for(int i = 0; i < 10; i++){
+            System.out.print("*");
+        }
+    }
+
+    public static void main(String[] args) {
+        // Please write your code here.
+        for(int i = 0; i < 5; i++){
+            print10Stars();
+            System.out.println();
+        }
+    }
+}
